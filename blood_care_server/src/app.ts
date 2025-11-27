949e9e900550b8fd8d72891084518c846b5b6d81
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-11-27 08:20:06 +0600

    camp related page design

--- blood_care_server/src/app.ts
+++ blood_care_server/src/app.ts
@@ -3,7 +3,9 @@ import express from 'express';
 const app = express();
 import { Request, Response } from 'express';
 import { bloodRequestRouter } from './modules/bloodRequest/bloodRequest.route';
+import cors from 'cors';
 app.use(express.json());
+app.use(cors())
 
 app.use("/api/v1/blood-requests", bloodRequestRouter);
 
